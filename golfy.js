4bfa249cc0c15dfab846bb5b7b7f47d960effa1b
Loustak <sardois.lucas@gmail.com>
2018-07-08 11:15:17 +0200

    Read file as utf8 text

--- golfy.js
+++ golfy.js
@@ -19,14 +19,18 @@ function de(b) {
     return g.join("");
 }
 
-fs.readFile('index.html', (err, data) => {
+fs.readFile('index.html', 'utf-8', (err, data) => {
 	if (err) throw err;
 
 	const lib = 'function de(b){var a,e={},d=b.split(""),c=f=d[0],g=[c],h=o=256;for(b=1;b<d.length;b++)a=d[b].charCodeAt(0),a=h>a?d[b]:e[a]?e[a]:f+c,g.push(a),c=a.charAt(0),e[o]=f+c,o++,f=a;return g.join("")}';
     const head = 'data:text/html;charset=utf-8;base64,';
-	const needed = '<script>' + lib + 'var a = window.location.href.substring(' + head.length + ');</script>'
+	const needed = '<script>' + lib + 'var a = window.location.href.substring(' + head.length + ');var b = atob(a);var c = b.substring(292);</script>'
 
-    const encodedData = encodeURIComponent(data);
+    const encodedData = en(data);
+    console.log('data base size: ' + data.length);
+    console.log('encoded data size: ' + encodedData.length);
+
+    //console.log(decodeURIComponent(de(decodeURIComponent(data))));
 
 	var url = head + Buffer.from(needed + encodedData).toString('base64');
 	clipboardy.writeSync(url);
